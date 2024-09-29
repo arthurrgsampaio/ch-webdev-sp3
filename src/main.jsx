87543f5d125d4b9c0a02adf3df47b73cf2dd8c36
Arthur Rodrigues <arthur.rgs03@gmail.com>
2024-09-29 20:25:47 -0300

    feat: Resgister page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import "./index.css";
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       {index:true, element: <Home />, path: '/'},
+      {path:'/registro', element: <Register/>},
       {path:'*', element: <PageNotFound/>}
     ]
   }

7c0cb349e53c457eb0d815e5162182e7f96089d5
Arthur Rodrigues <arthur.rgs03@gmail.com>
2024-09-29 21:24:04 -0300

    fix: ajuste no tailwind

--- src/pages/WIP.jsx
+++ src/pages/WIP.jsx
@@ -20,7 +20,7 @@ const WIP = () => {
                 inicial e explorar mais um pouco nosso site!
             </p>
             <button
-                className="text-white text-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-80 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white text-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-80 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 style={{ alignSelf: "end" }}
                 onClick={() => {navigate("/")}}
             >
